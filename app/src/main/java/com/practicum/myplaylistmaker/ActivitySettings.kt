5599ad3d741541bf21b96475da35551ea300bdc6
Mike Pavlov <7links91@mail.ru>
2023-04-23 22:34:42 +0300

    2

---
+++ app/src/main/java/com/practicum/myplaylistmaker/ActivitySettings.kt
@@ -0,0 +1,21 @@
+package com.practicum.myplaylistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.practicum.myapplication.MainActivity
+
+class ActivitySettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val returnButton = findViewById<ImageView>(R.id.return_button)
+        returnButton.setOnClickListener{
+            val intentBack = Intent(this@ActivitySettings,MainActivity::class.java)
+            startActivity(intentBack)
+        }
+
+    }
+}
--- app/src/main/java/com/practicum/myplaylistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/myplaylistmaker/MainActivity.kt
@@ -1,11 +1,36 @@
-package com.practicum.myplaylistmaker
+package com.practicum.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.practicum.myplaylistmaker.ActivityMedia
+import com.practicum.myplaylistmaker.ActivitySettings
+import com.practicum.myplaylistmaker.R
+import com.practicum.myplaylistmaker.SearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search)
+        val mediaButton = findViewById<Button>(R.id.media)
+        val settingsButton = findViewById<Button>(R.id.settings)
+
+        searchButton.setOnClickListener {
+            val intentSearch = Intent(this, SearchActivity::class.java)
+            startActivity(intentSearch)
+        }
+
+        mediaButton.setOnClickListener {
+            val intentMedia = Intent(this, ActivityMedia::class.java)
+            startActivity(intentMedia)
+        }
+
+        settingsButton.setOnClickListener {
+            val intentSettings = Intent(this, ActivitySettings::class.java)
+            startActivity(intentSettings)
+        }
     }
 }
