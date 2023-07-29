633d6bbf7ca67c85596665294309ae2fc06bc17c
Mike Pavlov <7links91@mail.ru>
2023-07-30 01:11:05 +0300

    12 sprint review.

--- app/src/main/java/com/practicum/myplaylistmaker/ActivitySettings.kt
+++ app/src/main/java/com/practicum/myplaylistmaker/ActivitySettings.kt
@@ -5,8 +5,6 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.Switch
-import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 const val PRACTICUM_EXAMPLE_PREFERENCES = "practicum_example_preferences"
