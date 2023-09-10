979b037a24bda574afec5ede4d35560155bce303
Mike Pavlov <7links91@mail.ru>
2023-09-10 19:16:43 +0300

    -

--- app/src/main/java/com/practicum/myplaylistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/myplaylistmaker/MainActivity.kt
@@ -1,31 +1,33 @@
 package com.practicum.myplaylistmaker
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.practicum.myplaylistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMain: ActivityMainBinding
+    private var sharedPrefs: SharedPreferences ? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sharedPrefs = getSharedPreferences(PRACTICUM_EXAMPLE_PREFERENCES,MODE_PRIVATE)
-        App().switchTheme(sharedPrefs.getBoolean(THEME_KEY,false))
-        val searchButton = findViewById<Button>(R.id.search)
-        val mediaButton = findViewById<Button>(R.id.media)
-        val settingsButton = findViewById<Button>(R.id.settings)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
 
-        searchButton.setOnClickListener {
+        sharedPrefs = getSharedPreferences(PRACTICUM_EXAMPLE_PREFERENCES,MODE_PRIVATE)
+        App().switchTheme(sharedPrefs?.getBoolean(THEME_KEY,false)!!)
+
+        bindingMain.search.setOnClickListener {
             val intentSearch = Intent(this, SearchActivity::class.java)
             startActivity(intentSearch)
         }
 
-        mediaButton.setOnClickListener {
+        bindingMain.media.setOnClickListener {
             val intentMedia = Intent(this, ActivityMedia::class.java)
             startActivity(intentMedia)
         }
 
-        settingsButton.setOnClickListener {
+        bindingMain.settings.setOnClickListener {
             val intentSettings = Intent(this, ActivitySettings::class.java)
             startActivity(intentSettings)
         }
