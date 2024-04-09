ef19409f36851ade48c133ff73ebb692d9d19a05
Mikhail.Pavlov <7links91@mail.ru>
2024-04-09 22:06:40 +0300

    22 спринт

--- app/src/main/java/com/practicum/myplaylistmaker/ui/search/adapter/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/myplaylistmaker/ui/search/adapter/TrackViewHolder.kt
@@ -8,14 +8,15 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.myplaylistmaker.R
 import com.practicum.myplaylistmaker.domain.models.Track
+import java.time.Duration
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val albumsCover: ImageView = itemView.findViewById(R.id.trackImage)
     val trackName: TextView = itemView.findViewById(R.id.trackName)
     val bandName: TextView = itemView.findViewById(R.id.groupName)
     val trackTime: TextView = itemView.findViewById(R.id.trackTime)
-    var trackNumber:Long = 0
+    var trackNumber: Long = 0
 
     fun bind(item: Track) {
         trackName.text = item.trackName
@@ -28,6 +29,16 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
             .transform(RoundedCorners(2))
             .into(albumsCover)
 
-        trackTime.text = ""//SimpleDateFormat("mm:ss", Locale.getDefault()).format(item.trackTimeMillis)
+        trackTime.text = formatMilliseconds(item.trackTimeMillis?.toLong()?:0)
     }
+
+
+    private fun formatMilliseconds(milliseconds: Long): String {
+        val duration = Duration.ofMillis(milliseconds)
+        val minutes = duration.toMinutes()
+        val seconds = duration.minusMinutes(minutes).seconds
+        return "$minutes:$seconds"
+    }
+
+
 }
